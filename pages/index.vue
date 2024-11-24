ee59665bb0b41691f62c8fb50df535b78c724b59
SergioIgnatev <ignatevsergey17@gmail.com>
2024-11-24 16:55:25 +0300

    sacha

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
   
     <button class="btn btn-success" @click="open=true">Открыть</button>
+    <button class="btn btn-danger" @click="open=false">Закрыть</button>
    <TheModalWindow
    :modal="mockFolders"
    :open="open" @pass-submit="open=false"/>
